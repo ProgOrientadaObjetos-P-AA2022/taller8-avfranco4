4be8d566f92117d74716961cc9610160bd75eada
avfranco4 <avfranco4@utpl.edu.ec>
2022-06-14 23:57:10 -0500

    Actualizacion

--- Taller08/src/paquete1/Docente.java
+++ Taller08/src/paquete1/Docente.java
@@ -1,21 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete1;
 
-/**
- *
- * @author reroes
- */
 public class Docente {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    protected String nombres;
+    protected String apellidos;
+    protected String identificacion;
+    protected int edad;
+
+    public Docente() {
+
     }
-    
+
+    public void establecerNombres(String c) {
+        nombres = c;
+    }
+
+    public String obtenerNombres() {
+        return nombres;
+    }
+
+    public String obtenerApellidos() {
+        return apellidos;
+    }
+
+    public void establecerIdentificacion(String c) {
+        identificacion = c;
+    }
+
+    public String obtenerIdentificacion() {
+        return identificacion;
+    }
+
+    public int obtenerEdad() {
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nombre: %s\nApellido: %s\nIdentificacion: %s\nEdad: %d\n", obtenerNombres(),
+                 obtenerApellidos(), obtenerIdentificacion(), obtenerEdad());
+    }
+
 }
